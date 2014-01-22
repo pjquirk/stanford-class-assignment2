535dc438cd6ac5ccab1c4d38f934c18dc0e3432c
Patrick Quirk <quirker@gmail.com>
2014-01-21 21:10:03 -0500

    Implemented chooseCardAtIndex

--- Matchismo/Matchismo/Model/CardMatchingGame.m
+++ Matchismo/Matchismo/Model/CardMatchingGame.m
@@ -47,12 +47,42 @@
     return (index < [self.cards count]) ? self.cards[index] : nil;
 }
 
+static const int MISMATCH_PENALTY = 2;
+static const int MATCH_BONUS = 4;
+static const int COST_TO_CHOOSE = 1;
+
 -(void)chooseCardAtIndex:(NSUInteger)index
 {
     Card* card = [self cardAtIndex:index];
     if (card)
     {
-        
+        if (!card.isMatched)
+        {
+            if (card.chosen)
+            {
+                for (Card* otherCard in self.cards) {
+                    if (otherCard.isChosen && !otherCard.isMatched)
+                    {
+                        int matchScore = [card match:@[otherCard]];
+                        if (matchScore)
+                        {
+                            self.score += matchScore * MATCH_BONUS;
+                            card.matched = YES;
+                            otherCard.matched = YES;
+                        }
+                        else
+                        {
+                            self.score -= MISMATCH_PENALTY;
+                            otherCard.chosen = NO;
+                        }
+                        break;
+                    }
+                }
+                self.score -= COST_TO_CHOOSE;
+            }
+            
+            card.chosen = !card.isChosen;
+        }
     }
 }
 
